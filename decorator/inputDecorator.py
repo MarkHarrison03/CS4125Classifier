07022ff729f4898673f53bd545b0a25cd3ffaf36
Mark <21335265@studentmail.ul.ie>
2024-11-23 23:02:46 +0000

    added remove noise decorator

--- decorator/inputDecorator.py
+++ decorator/inputDecorator.py
@@ -1,7 +1,7 @@
 import functools
 from googletrans import Translator
 from userSettings import userSettings
-
+import re
  
 
 class inputDecorator:
@@ -10,6 +10,53 @@ class inputDecorator:
         self.translator = Translator()
         self.target_language = target_language
         
+
+    def remove_noise(self, text):
+            noise_1 = [
+                r"\d{2}(:|.)\d{2}",
+                r"(xxxxx@xxxx\.com)|(\*{5}\([a-z]+\))",
+                r"dear ((customer)|(user))",
+                r"dear",
+                r"(hello)|(hallo)|(hi )|(hi there)",
+                r"good morning",
+                r"thank you for your patience ((during (our)? investigation)|(and cooperation))?",
+                r"thank you for contacting us",
+                r"thank you for your availability",
+                r"thank you for providing us this information",
+                r"thank you for contacting",
+                r"thank you for reaching us (back)?",
+                r"thank you for patience",
+                r"thank you for (your)? reply",
+                r"thank you for (your)? response",
+                r"thank you for (your)? cooperation",
+                r"thank you for providing us with more information",
+                r"thank you very kindly",
+                r"thank you( very much)?",
+                r"i would like to follow up on the case you raised on the date",
+                r"i will do my very best to assist you",
+                r"in order to give you the best solution",
+                r"could you please clarify your request with following information:",
+                r"in this matter",
+                r"we hope you(( are)|('re)) doing ((fine)|(well))",
+                r"i would like to follow up on the case you raised on",
+                r"we apologize for the inconvenience",
+                r"sent from my huawei (cell )?phone",
+                r"original message",
+                r"customer support team",
+                r"(aspiegel )?se is a company incorporated under the laws of ireland with its headquarters in dublin, ireland.",
+                r"(aspiegel )?se is the provider of huawei mobile services to huawei and honor device owners in",
+                r"canada, australia, new zealand and other countries",
+                r"\d+",
+                r"[^0-9a-zA-Z]+",
+                r"(\s|^).(\s|$)"
+            ]
+            
+            combined_pattern = "|".join(noise_1)
+            cleaned_text = re.sub(combined_pattern, "", text.lower())
+            cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip()
+            print( "cleaned text", cleaned_text)
+
+            return cleaned_text
         
     def translate_text(self, text):
         """Uses translation API to translate the text."""
@@ -26,20 +73,30 @@ class inputDecorator:
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            user_settings = userSettings()   
+            subject = input("Please enter the subject line: ").strip()
+            email = input("Please enter the email body text: ").strip()
+            
+            user_settings = userSettings() 
+            print(user_settings)  
             self.translate = user_settings.translate_text
-            print(user_settings)
-            print("Aaa!")
-            print(args, kwargs)
-            print(self.translate)
-            if self.translate:
-                subject = input("Please enter the subject line: ").strip()
-                email = input("Please enter the email body text: ").strip()
+            self.removeNoise = user_settings.remove_noise
+            print(self.translate, self.removeNoise)
 
-                translated_subject = self.translate_text(subject)
-                translated_body = self.translate_text(email)
+            
+            if self.translate == True:
+                print("translated!")
+                subject = self.translate_text(subject).text
+                email = self.translate_text(email).text
                 
-                return func(translated_subject.text, translated_body.text, *args, **kwargs)
-            return func(*args, **kwargs)
+            if self.removeNoise == True:
+                subject = self.remove_noise(subject)
+                email = self.remove_noise(email)
+                print("Noise removed")
+                print(subject, email)        
+                return func(subject, email, *args, **kwargs)
+       
+
+            return func(subject, email, *args, **kwargs)
+
         
         return wrapper
